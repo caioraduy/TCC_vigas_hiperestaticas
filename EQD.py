1b6dfd1cd72b88e9d72ad9ae69f7f2203d59d10d
caioraduy <raduycaio@gmail.com>
2023-04-16 16:30:32 -0300

    GENERALIZA CÁLCULO DAS REAÇÕES DE APOIO, MOMENTOS E EQUAÇÕES DOS MOMENTOS

--- EQD.py
+++ EQD.py
@@ -15,14 +15,14 @@ class Calcula_momentos_por_trecho(Vigahiperestatica):
         Vigahiperestatica.__init__(self, viga)
     def calcula_comprimentos_acumulados(self):
         self.lista_L_acumulados = []
-        print(self.viga.lista_comprimentos)
+        #print(self.viga.lista_comprimentos)
         for i in range (0,len(self.viga.lista_comprimentos)):
             if i ==0:
                 self.comprimento_acumulado=0
             else:
                 self.comprimento_acumulado = self.comprimento_acumulado + self.viga.lista_comprimentos[i]
             self.lista_L_acumulados.append(self.comprimento_acumulado)
-        print(self.lista_L_acumulados)
+        #print(self.lista_L_acumulados)
     def gera_equacoes_momentos_por_trecho(self):
         self.lista_eq_LE=[]
         L = 0
@@ -59,7 +59,8 @@ class Calcula_momentos_por_trecho(Vigahiperestatica):
             LE.append(self.x_acumulado)
             LE.append(x_2)
             self.lista_eq_LE.append(LE)
-        print(self.lista_eq_LE)
+        print("Os polinômios que representam a equação dos momentos por trecho são")
+        print("(do último para o primeiro trecho):",self.lista_eq_LE)
     def apply(self):
         self.calcula_comprimentos_acumulados()
         self.gera_equacoes_momentos_por_trecho()
@@ -85,6 +86,8 @@ class Eq3momentos(Vigahiperestatica):
         self.matriz_incognitas = None
         self.lista_momentos = None
         self.lista_reacoes = None
+        self.lista_reacoes = None
+        self.matriz_reacoes = None
 
         Vigahiperestatica.__init__(self, viga)
     def gera_matriz_cheia_de_zeros(self):
@@ -95,7 +98,7 @@ class Eq3momentos(Vigahiperestatica):
             for j in range(0, len(self.viga.lista_comprimentos) + 1):
                 x = 0
                 self.lista_incognitas_vazia.append(x)
-            print(self.lista_incognitas_vazia)
+            #print(self.lista_incognitas_vazia)
             self.matriz_incognitas_vazia.append(self.lista_incognitas_vazia)
     def adiciona_elementos_na_matriz_das_incognitas(self):
         self.lista_incognitas_prencher[self.indice_mi_anterior] = self.Mi_anterior
@@ -122,7 +125,7 @@ class Eq3momentos(Vigahiperestatica):
             self.lista_momentos.append(self.Resultados_momentos[i][0])
         self.lista_momentos.insert(0,0)
         self.lista_momentos.insert(len(self.viga.lista_comprimentos),0)
-        print(self.lista_momentos)
+
 
     def equacao_3_momentos(self):
         self.gera_matriz_cheia_de_zeros()
@@ -148,7 +151,6 @@ class Eq3momentos(Vigahiperestatica):
                 self.Mi_posterior = 0
             # se i> 0 e não é último apoio que estamos tratando
             if i > 0 and i <  len(self.viga.lista_comprimentos)-2:
-                #print('arromba')
                 self.Mi_anterior = self.viga.lista_comprimentos[i]
                 self.Mi = 2 * self.viga.lista_comprimentos[i] + 2 * self.viga.lista_comprimentos[i+1]
                 self.Mi_posterior = self.viga.lista_comprimentos[i + 1]
@@ -170,57 +172,57 @@ class Eq3momentos(Vigahiperestatica):
         self.gera_matriz_quadrada()
         #resolve o sistema de equações
         self.resolve_sistema_equacoes()
-        print(self.Resultados_momentos)
+        print(f' Os momentos nos apoios são: {self.Resultados_momentos}')
         self.gera_lista_momentos()
         self.calcula_reacoes_apoio()
 
     def calcula_reacoes_apoio(self):
-        lista_reacoes =[]
-        matriz_reacoes =[]
+        self.lista_reacoes =[]
+        self.matriz_reacoes =[]
+        #CALCULA AS REAÇÕES DE APOIO POR TRECHO E ADICIONA EM UMA MATRIZ COM OS PARES
         for i in range( 0,len(self.viga.lista_comprimentos)):
             lista_reacoes_esquerda_direita =[]
+            #MOMENTO GERADO PELA CARGA
             parcela_carga = self.viga.carga_q * self.viga.lista_comprimentos[i]*self.viga.lista_comprimentos[i]/2
+            #MOMENTO APOIO DA ESQUERDA
             parcela_momento_i = self.lista_momentos[i]
+            #MOMENTO APOIO DA DIREITA
             parcela_momento_i_mais1 = -1*self.lista_momentos[i+1]
+            # REAÇÃO DE APOIO DA DIREITA
             Ri_mais1 = (parcela_carga + parcela_momento_i +parcela_momento_i_mais1)/self.viga.lista_comprimentos[i]
+            # REAÇÃO DE APOIO DA ESQUERDA
             Ri = self.viga.carga_q*self.viga.lista_comprimentos[i] - Ri_mais1
+            # ADICIONA OS PARES DE REAÇÃO
             lista_reacoes_esquerda_direita.append(Ri)
             lista_reacoes_esquerda_direita.append(Ri_mais1)
-            matriz_reacoes.append(lista_reacoes_esquerda_direita)
-        print(matriz_reacoes)
-        print(len(matriz_reacoes))
-        for i in range(0, len(matriz_reacoes)-1):
+            self.matriz_reacoes.append(lista_reacoes_esquerda_direita)
+        # SOMA A REAÇÃO DA ESQUERDA COM A REAÇÃO DA DIREITA
+        for i in range(0, len(self.matriz_reacoes)):
+            # SE I ==0 E A VIGA TEM MAIS DE 2 TRAMOS (MAIS DE 3 APOIOS)
             if i == 0 and len(self.viga.lista_comprimentos) >2:
-                r_acumulado_i = matriz_reacoes[i][0]
-                r_acumulado_i_mais_1 = matriz_reacoes[i][1] + matriz_reacoes[i + 1][0]
-                lista_reacoes.append(r_acumulado_i)
-                lista_reacoes.append(r_acumulado_i_mais_1)
-            elif i == len(matriz_reacoes)-1:
-                r_acumulado_i_mais_1 = matriz_reacoes[i][1]
-                lista_reacoes.append(r_acumulado_i_mais_1)
+                r_acumulado_i = self.matriz_reacoes[i][0]
+                r_acumulado_i_mais_1 = self.matriz_reacoes[i][1] + self.matriz_reacoes[i + 1][0]
+                self.lista_reacoes.append(r_acumulado_i)
+                self.lista_reacoes.append(r_acumulado_i_mais_1)
+            # SE ESTAMOS NO ÚLTIMO TRECHO DA VIGA
+            elif i == len(self.matriz_reacoes)-1:
+                r_acumulado_i_mais_1 = self.matriz_reacoes[i][1]
+                self.lista_reacoes.append(r_acumulado_i_mais_1)
+            # SE I =0 E A VIGA TEM APENAS DOIS TRAMOS (3 APOIOS)
             elif  i == 0 and len(self.viga.lista_comprimentos) == 2:
-                r_acumulado_i_menos_1 = matriz_reacoes[i][0]
-                r_acumulado_i = matriz_reacoes[i][1] + matriz_reacoes[i + 1][0]
-                r_acumulado_i_mais_1 = matriz_reacoes[i+1][0]
-                lista_reacoes.append(r_acumulado_i_menos_1)
-                lista_reacoes.append(r_acumulado_i)
-                lista_reacoes.append(r_acumulado_i_mais_1)
-
+                r_acumulado_i_menos_1 = self.matriz_reacoes[i][0]
+                r_acumulado_i = self.matriz_reacoes[i][1] + self.matriz_reacoes[i + 1][0]
+                r_acumulado_i_mais_1 = self.matriz_reacoes[i+1][0]
+                self.lista_reacoes.append(r_acumulado_i_menos_1)
+                self.lista_reacoes.append(r_acumulado_i)
+                self.lista_reacoes.append(r_acumulado_i_mais_1)
+            # SE NÃO ESTAMOS NOS TRECHOS DAS EXTREMIDADE DAS VIGAS
             else:
-                r_acumulado_i = matriz_reacoes[i][1]+ matriz_reacoes[i+1][0]
-                lista_reacoes.append(r_acumulado_i)
+                r_acumulado_i = self.matriz_reacoes[i][1]+ self.matriz_reacoes[i+1][0]
+                self.lista_reacoes.append(r_acumulado_i)
+        self.viga.lista_reações =self.lista_reacoes
 
-
-        print( 'xxxxxxxx', lista_reacoes)
-
-
-
-
-
-
-
-
-
+        print( f' O vetor com as reações de apoio da viga é: {self.lista_reacoes}')
 
 
 
@@ -237,14 +239,15 @@ class Contexto:
     def __init__(self,viga):
         self.viga= viga
     def apply(self):
-        momentos = Calcula_momentos_por_trecho(self.viga)
-        momentos.apply()
         reações = Eq3momentos(self.viga)
         reações.apply()
+        momentos = Calcula_momentos_por_trecho(self.viga)
+        momentos.apply()
+
 
 if __name__== '__main__':
-    viga = Vigahiperestatica(lista_comprimentos=[10,10],carga_q=1
-                             ,lista_reações=[1,1,2,1]
+    viga = Vigahiperestatica(lista_comprimentos=[10,10,10,10],carga_q=1
+
     )
     contexto = Contexto(viga)
     contexto.apply()
